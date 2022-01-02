cbb5cd46094e3b9c7acf7a5fe39f7c3e4ed5329c
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-01-02 16:51:06 +0300

    Asp.Net Core Mvc-1 Uygulaması ile API1’den veri almak(token ile)

---
+++ Client1/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Client1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProductsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+
+            var discovery = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (discovery.IsError)
+            {
+                //loglama yap
+            }
+
+            var token = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest()
+            {
+                ClientId = _configuration["Client:ClientId"],
+                ClientSecret = _configuration["Client:ClientSecret"],
+                Address = discovery.TokenEndpoint
+            });
+
+            if (token.IsError)
+            {
+                //loglama yap
+            }
+
+            //https://localhost:5016
+
+            client.SetBearerToken(token.AccessToken);
+
+            var response = await client.GetAsync("https://localhost:5016/api/Products/GetProducts");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                //loglama yap
+            }
+
+            return View();
+        }
+    }
+}
--- UdemyIdentityServer.AuthServer/Config.cs
+++ UdemyIdentityServer.AuthServer/Config.cs
@@ -9,8 +9,22 @@ namespace UdemyIdentityServer.AuthServer
         {
             return new List<ApiResource>()
             {
-                new ApiResource("resource_api1"){Scopes = {"api1.read", "api1.write","api1.update"}},
-                new ApiResource("resource_api2"){Scopes = {"api2.read", "api2.write","api2.update"}}
+                new ApiResource("resource_api1")
+                {
+                    Scopes = {"api1.read", "api1.write","api1.update"},
+                    ApiSecrets = new List<Secret>()
+                    {
+                        new Secret("secretapi1".Sha256())
+                    }
+                },
+                new ApiResource("resource_api2")
+                {
+                    Scopes = {"api2.read", "api2.write","api2.update"},
+                    ApiSecrets = new List<Secret>()
+                    {
+                        new Secret("secretapi2".Sha256())
+                    }
+                }
             };
         }
 
