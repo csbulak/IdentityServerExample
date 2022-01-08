b01ea1ecedf4bab317c6144f4ec7e4de312620f1
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-01-08 18:21:12 +0300

    47. Custom Üyelik Sistemi-3(Merkezi)

---
+++ UdemyIdentityServer.AuthServer/Services/ICustomUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using UdemyIdentityServer.AuthServer.Models;
+
+namespace UdemyIdentityServer.AuthServer.Services
+{
+    public interface ICustomUserRepository
+    {
+        Task<bool> Validate(string email, string password);
+        Task<CustomUser> FindById(int id);
+        Task<CustomUser> FindByEmail(string email);
+    }
+}
