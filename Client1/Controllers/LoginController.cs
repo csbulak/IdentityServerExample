e70959102b88fa64a5576f34c2411148741a9354
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-01-10 18:14:49 +0300

    Login Sayfası Oluşturmak

---
+++ Client1/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
