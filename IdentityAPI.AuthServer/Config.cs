25bd2fb4c0694357dac639db1c48d53a2f6cabfe
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-01-11 14:44:43 +0300

    71. IdentityServer4 ASP.NET Core Identity -2

--- IdentityAPI.AuthServer/Config.cs
+++ IdentityAPI.AuthServer/Config.cs
@@ -2,57 +2,214 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using System;
 using IdentityServer4.Models;
 using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityServer4;
+using IdentityServer4.Test;
 
 namespace IdentityAPI.AuthServer
 {
     public static class Config
     {
-        public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
-                   };
-
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>()
             {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
-            };
-
-        public static IEnumerable<Client> Clients =>
-            new Client[]
-            {
-                // m2m client credentials flow client
-                new Client
+                new ApiResource("resource_api1")
                 {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
+                    Scopes = {"api1.read", "api1.write","api1.update"},
+                    ApiSecrets = new List<Secret>()
+                    {
+                        new Secret("secretapi1".Sha256())
+                    }
+                },
+                new ApiResource("resource_api2")
+                {
+                    Scopes = {"api2.read", "api2.write","api2.update"},
+                    ApiSecrets = new List<Secret>()
+                    {
+                        new Secret("secretapi2".Sha256())
+                    }
+                }
+            };
+        }
 
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new List<ApiScope>()
+            {
+                new ApiScope("api1.read", "API 1 için okuma izni"),
+                new ApiScope("api1.write", "API 1 için yazma izni"),
+                new ApiScope("api1.update", "API 1 için güncelleme izni"),
+                new ApiScope("api2.read", "API 2 için okuma izni"),
+                new ApiScope("api2.write", "API 2 için yazma izni"),
+                new ApiScope("api2.update", "API 2 için güncelleme izni")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>()
+            {
+                new Client()
+                {
+                    ClientId = "Client1",
+                    ClientName = "Client 1 Web App",
+                    ClientSecrets = new List<Secret>()
+                    {
+                        new Secret("secret".Sha256())
+                    },
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
+                    AllowedScopes = new List<string>()
+                    {
+                        "api1.read"
+                    }
                 },
-
-                // interactive client using code flow + pkce
-                new Client
+                new Client()
                 {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
+                    ClientId = "Client2",
+                    ClientName = "Client 2 Web App",
+                    ClientSecrets = new List<Secret>()
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = new List<string>()
+                    {
+                        "api1.read",
+                        "api1.update",
+                        "api2.write",
+                        "api2.update"
+                    }
                 },
+                new Client()
+                {
+                    ClientId = "Client1-Mvc",
+                    RequirePkce = false,
+                    ClientName = "Client 1 Mvc App",
+                    ClientSecrets = new List<Secret>()
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    RedirectUris = new List<string>()
+                    {
+                        "https://localhost:5006/signin-oidc"
+                    },
+                    PostLogoutRedirectUris = new List<string>
+                    {
+                        "https://localhost:5006/signout-callback-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api1.read",
+                        IdentityServerConstants.StandardScopes.OfflineAccess,
+                        "CountryAndCity",
+                        "Roles",
+                        IdentityServerConstants.StandardScopes.Email
+                    },
+                    AccessTokenLifetime = 2*60*60,
+                    AllowOfflineAccess = true,
+                    RefreshTokenUsage = TokenUsage.ReUse,
+                    RefreshTokenExpiration = TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime = (int)(DateTime.Now.AddDays(60) - DateTime.Now).TotalSeconds,
+                    RequireConsent = false
+                },
+                new Client()
+                {
+                    ClientId = "Client2-Mvc",
+                    RequirePkce = false,
+                    ClientName = "Client 2 Mvc App",
+                    ClientSecrets = new List<Secret>()
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    RedirectUris = new List<string>()
+                    {
+                        "https://localhost:5011/signin-oidc"
+                    },
+                    PostLogoutRedirectUris = new List<string>
+                    {
+                        "https://localhost:5011/signout-callback-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api1.read",
+                        IdentityServerConstants.StandardScopes.OfflineAccess,
+                        "CountryAndCity",
+                        "Roles"
+                    },
+                    AccessTokenLifetime = 2*60*60,
+                    AllowOfflineAccess = true,
+                    RefreshTokenUsage = TokenUsage.ReUse,
+                    RefreshTokenExpiration = TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime = (int)(DateTime.Now.AddDays(60) - DateTime.Now).TotalSeconds,
+                    RequireConsent = false
+                },
+                new Client()
+                {
+                    ClientId = "Client1-ResourceOwner-Mvc",
+                    ClientName = "Client 1 Mvc App",
+                    ClientSecrets = new List<Secret>()
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api1.read",
+                        IdentityServerConstants.StandardScopes.OfflineAccess,
+                        "CountryAndCity",
+                        "Roles",
+                        IdentityServerConstants.StandardScopes.Email
+                    },
+                    AccessTokenLifetime = 2*60*60,
+                    AllowOfflineAccess = true,
+                    RefreshTokenUsage = TokenUsage.ReUse,
+                    RefreshTokenExpiration = TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime = (int)(DateTime.Now.AddDays(60) - DateTime.Now).TotalSeconds,
+                }
             };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>()
+            {
+                new IdentityResources.Email(),
+                new IdentityResources.OpenId(), // Token İçinde Kullanıcının ID'si => subID
+                new IdentityResources.Profile(), // Kullanıcı Hakkında Claim
+                new IdentityResource()
+                {
+                    Name = "CountryAndCity",
+                    DisplayName = "Country and City",
+                    Description = "Kullanıcının ülke ve şehir bilgisi",
+                    UserClaims = new List<string>()
+                    {
+                        "country",
+                        "city"
+                    }
+                },
+                new IdentityResource()
+                {
+                    Name = "Roles",
+                    DisplayName = "Roles",
+                    Description = "Kullanıcı Rolleri",
+                    UserClaims = new List<string>()
+                    {
+                        "role"
+                    }
+                }
+            };
+        }
     }
 }
--- IdentityAPI.AuthServer/Startup.cs
+++ IdentityAPI.AuthServer/Startup.cs
@@ -31,7 +31,7 @@ namespace IdentityAPI.AuthServer
             services.AddControllersWithViews();
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
@@ -47,9 +47,10 @@ namespace IdentityAPI.AuthServer
                 // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
                 options.EmitStaticAudienceClaim = true;
             })
-                .AddInMemoryIdentityResources(Config.IdentityResources)
-                .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryClients(Config.Clients)
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiScopes(Config.GetApiScopes())
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients())
                 .AddAspNetIdentity<ApplicationUser>();
 
             // not recommended for production - you need to store your key material somewhere secure
@@ -59,7 +60,7 @@ namespace IdentityAPI.AuthServer
                 .AddGoogle(options =>
                 {
                     options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-                    
+
                     // register your IdentityServer with Google at https://console.developers.google.com
                     // enable the Google+ API
                     // set the redirect URI to https://localhost:5001/signin-google
